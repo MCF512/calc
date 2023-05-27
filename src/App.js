6b374c76d71cb4ac9ed6a4f42c6523c0d36cb4ea
MCF512 <egdem4enko@yandex.ru>
2023-05-27 19:56:18 +0300

    done calc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import st from './App.module.css';
+
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '+', '-', 'c', '='];
 
 function App() {
+  let [result, setResult] = useState(0);
+  let [final, setFinal] = useState(false);
+
+  function handleClick(val) {
+    if (result == 0) {
+      if (val == 0 || val == '+' || val == '-' || val == '=' || val == 'c') {
+        setResult(0)
+      } else {
+        setResult(val)
+      }
+    } else {
+      if ((result[result.length - 1] == '+' || result[result.length - 1] == '-') && (val == '+' || val == '-')) {
+        return
+      } else if (val == 'c') {
+        setFinal(false)
+        setResult(0)
+      } else if (val == '=') {
+        setFinal(!final)
+        setResult(eval(result));
+      } else {
+        if (final) {
+          setFinal(!final)
+        }
+        setResult(result += `${val}`)
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={st.App}>
+      <div className={st.container}>
+        <p className={final ? st.final : st.result}>{result}</p>
+
+        {nums.map((num) => {
+          return <button className={st.btn} onClick={() => handleClick(num)} key={num}>
+            {num}
+          </button>
+        })}
+      </div>
     </div>
   );
 }
